1c9daeeb79fbd073e4aa38ea957ad534cfa0a630
Michael <michael.a@compedia.net>
2018-01-25 22:44:31 +0200

    first rotation controller push

---
+++ GGJ2018/Assets/Scripts/DiscRotationController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscRotationController : MonoBehaviour 
+{
+    [SerializeField]
+    [Range(10,1000)]
+    private float m_rotationSpeed;
+
+    // Update is called once per frame
+    private void Update () 
+    {
+        float axis = Input.GetAxis("Horizontal");
+        transform.Rotate(0, 0, Input.GetAxis("Horizontal") * m_rotationSpeed * Time.deltaTime);
+    }
+}
