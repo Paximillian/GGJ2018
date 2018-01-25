12191182792b739724ecb4be69e6e1d8911e05a3
ASUSVR\Compedia <morzil@walla.com>
2018-01-26 01:18:41 +0200

    no message

--- GGJ2018/Assets/Scripts/DiscRotationController.cs
+++ GGJ2018/Assets/Scripts/DiscRotationController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DiscRotationController : MonoBehaviour 
+public class DiscRotationController : MonoBehaviour
 {
-    public enum ControllerAxis 
+    public enum ControllerAxis
     {
         HorizontalAxis1,
         HorizontalAxis2,
@@ -13,8 +14,23 @@ public class DiscRotationController : MonoBehaviour
         HorizontalAxis4,
     }
 
+    private int m_Points;
+    public int Points
+    {
+        get
+        {
+            return m_Points;
+        }
+        set
+        {
+            m_MahPointz.text = string.Format("Player {0}: {1}", 
+                int.Parse(m_controllerAxis.ToString().Last().ToString()),
+                m_Points = value);
+        }
+    }
+
     [SerializeField]
-    [Range(10,1000)]
+    [Range(10, 1000)]
     private float m_rotationSpeed;
 
     [SerializeField]
@@ -25,15 +41,28 @@ public class DiscRotationController : MonoBehaviour
 
     private string m_controllerAxisName;
 
-    private void Awake() 
+    private void Awake()
     {
-        m_controllerAxisName = m_controllerAxis.ToString();  
+        m_controllerAxisName = m_controllerAxis.ToString();
+
+        m_MahPointz.transform.SetParent(ScoreContainer.Instance.transform);
+
+        Points = 0;
     }
-    
-    private void Update () 
+
+    private void Update()
     {
         transform.Rotate(0, 0, Input.GetAxis(m_controllerAxisName) * m_rotationSpeed * Time.deltaTime * -1);
 
         //transform.
+    }
+
+    private void OnTriggerEnter(Collider oucher)
+    {
+        if(oucher.gameObject.layer == LayerMask.NameToLayer("Bulleter"))
+        {
+            Points--;
+            oucher.gameObject.SetActive(false);
+        }
     }
 }
