9e64663c312610139a5a217adc0268eb435ea82a
ASUSVR\Compedia <morzil@walla.com>
2018-01-26 00:06:14 +0200

    no message

--- GGJ2018/Assets/Scripts/DiscRotationController.cs
+++ GGJ2018/Assets/Scripts/DiscRotationController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiscRotationController : MonoBehaviour 
 {
@@ -18,6 +19,9 @@ public class DiscRotationController : MonoBehaviour
 
     [SerializeField]
     private ControllerAxis m_controllerAxis;
+
+    [SerializeField]
+    private Text m_MahPointz;
 
     private string m_controllerAxisName;
 
