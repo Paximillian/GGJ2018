7fb2d423cf718848af239779fa61eb21a1146d1e
ASUSVR\Compedia <morzil@walla.com>
2018-01-25 21:43:41 +0200

    - Added object pooling.

---
+++ GGJ2018/Assets/ObjectPool/Object Pooling/IGameObjectPool.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IGameObjectPool
+{
+    void AddSource(GameObject i_GameObjForPool, int i_Amount = 1);
+    GameObject PullObject();
+}
---
+++ GGJ2018/Assets/ObjectPool/Object Pooling/IObjectPoolManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IObjectPoolManager
+{
+    IGameObjectPool GetPoolForObject(string i_ObjectPoolTag);
+    GameObject GetObject(string i_ObjectPoolTag);
+}
