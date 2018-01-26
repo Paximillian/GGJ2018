d8d175cef94c59b59a27a0f871ca28d6b77827f3
Memilmon <emilatron@gmail.com>
2018-01-27 01:24:58 +0200

    random buffs added

--- GGJ2018/Assets/Scripts/RouteMakerBetterers/MakerBettererBox.cs
+++ GGJ2018/Assets/Scripts/RouteMakerBetterers/MakerBettererBox.cs
@@ -11,7 +11,17 @@ public class MakerBettererBox : RadioWaveController
 
     private void OnEnable()
     {
-        insides = new BulleterFasterer();
+        int numberOfBetererer = Random.Range(0, 2);
+        switch (numberOfBetererer) {
+            case 0:
+                insides = new BulleterFasterer();
+                break;
+            case 1:
+                insides = new WaeSlowerer();
+                break;
+            default:
+                break;
+        }
         m_SplodingParticles.Play();
     }
 }
