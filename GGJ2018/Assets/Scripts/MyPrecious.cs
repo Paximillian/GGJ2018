58de33b4f3f7a7202f0d7dfb14674d137726b7a1
xelanos <or3232@gmail.com>
2018-01-26 23:06:45 +0200

    points to win implemented

--- GGJ2018/Assets/Scripts/MyPrecious.cs
+++ GGJ2018/Assets/Scripts/MyPrecious.cs
@@ -9,9 +9,20 @@ public class MyPrecious : Singleton<MyPrecious>
     [Range(1, 4)]
     private int m_numberOfPlayers;
 
+    [SerializeField]
+    [Range(1, 100)]
+    private int m_pointsToWin;
+
+
     public int NumberOfPlayers 
     {
         get { return m_numberOfPlayers; }
         set { m_numberOfPlayers = value; }
+    }
+
+    public int PointsToWin
+    {
+        get { return m_pointsToWin; }
+        set { m_pointsToWin = value; }
     }
 }
---
+++ GGJ2018/Assets/Scripts/PointsFromSlider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsFromSlider : MonoBehaviour {
+
+    public Slider Points;
+    public Text self;
+
+    public void updateText()
+    {
+        self.text = Points.value.ToString();
+    }
+}
