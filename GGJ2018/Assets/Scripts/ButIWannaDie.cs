fa6968bd3d65105c5d6a44f4e58465fbd07ecad3
Michael <michael.a@compedia.net>
2018-01-27 15:54:08 +0200

    you can now play agan

--- GGJ2018/Assets/Scripts/ButIWannaDie.cs
+++ GGJ2018/Assets/Scripts/ButIWannaDie.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class ButIWannaDie : MonoBehaviour
 {
+    static bool initialized = false;
+
     // Use this for initialization
     void Start()
     {
+        if (initialized == false) {
+            initialized = true;
         DontDestroyOnLoad(gameObject);
+        }
+        else { Destroy(this.gameObject); }
+        
+
     }
 }
